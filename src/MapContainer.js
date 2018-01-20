e87e988ec2d13ebb49ff7d0419935cd8b3f8d01a
Salma Mohammed <sm.71175@gmail.com>
2018-01-20 05:18:11 +0200

    Add Markers

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import MapComp from './MapComp';
+import {Map, InfoWindow, Marker, Polygon, GoogleApiWrapper} from 'google-maps-react';
 import './App.css';
 
 export class MapContainer extends React.Component {
+
   render() {
+    const style = {
+      width: '100%',
+      height: '100%'
+    }
+
+    const places = [
+      {lat: 30.0303311, lng: 31.284036}, // Delavija+Sama+Restaurants
+      {lat: 30.0566282, lng: 31.3213528}, // Genena Mall
+      {lat: 30.056331, lng: 31.3265885}, // Hosny Restaurant
+      {lat: 30.0522079, lng: 31.3302792}, // Wonder Land
+      {lat: 30.0522079, lng: 31.3302792}, // McDonald
+      {lat: 30.0729836, lng: 31.3416726} // City Stars
+    ]
+
+    let markers = places.map((place, index) => {
+      return(
+        <Marker
+        key={index}
+        title={index}
+        name={'SOMA'}
+        position={places[index]} />
+      )
+    })
 
     return (
-      <Map google={this.props.google} zoom={14}>
-
+      <Map google={this.props.google}
+      zoom={14} style={style} initialCenter={{lat: 30.0566282, lng: 31.3213528}}
+      >
+      {markers}
       </Map>
     )
   }
